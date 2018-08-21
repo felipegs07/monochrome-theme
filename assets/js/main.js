1c7674fbc1454214e1460244d4aca1bd22bc5083
Felipe <felipegdas07@gmail.com>
2018-08-21 18:36:18 -0300

    home page bio ready, link to projects need fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,9 @@
+//variables
 const rightSide = document.querySelector('.right');
+const portfolio = document.querySelector('.portfolio');
+const btnPortfolio = document.querySelector('#btn-portfolio');
 
-
+//event listeners
 rightSide.addEventListener('mouseenter', () =>{
     rightSide.classList.add('hover-right');
     rightSide.classList.remove('hover-off');
@@ -10,3 +13,14 @@ rightSide.addEventListener('mouseleave', () =>{
     rightSide.classList.remove('hover-right');
     rightSide.classList.add('hover-off');
 });
+
+btnPortfolio.addEventListener('click', () => {
+    portfolio.classList.add('portfolio-fadein');
+    btnClose();
+});
+
+
+//functions
+function btnClose(){
+    btnPortfolio.style.display = 'none';
+}
