bcd338666cbfd91265c0804f17ccefb17d536d29
Felipe <felipegdas07@gmail.com>
2018-08-23 18:16:44 -0300

    home ready

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -1,26 +1,52 @@
+console.log('Hello my friend ;)');
 //variables
 const rightSide = document.querySelector('.right');
-const portfolio = document.querySelector('.portfolio');
+const portfolio = document.querySelector('#portfolio');
 const btnPortfolio = document.querySelector('#btn-portfolio');
-
-//event listeners
-rightSide.addEventListener('mouseenter', () =>{
-    rightSide.classList.add('hover-right');
-    rightSide.classList.remove('hover-off');
-});
-
-rightSide.addEventListener('mouseleave', () =>{
-    rightSide.classList.remove('hover-right');
-    rightSide.classList.add('hover-off');
-});
-
-btnPortfolio.addEventListener('click', () => {
-    portfolio.classList.add('portfolio-fadein');
-    btnClose();
-});
+const blog = document.querySelector('#posts');
+const btnBlog = document.querySelector('#btn-blog');
+const btnCloseModal = document.querySelector('.close-btn');
 
 
 //functions
 function btnClose(){
     btnPortfolio.style.display = 'none';
 }
+
+function showModal(modalID){
+    modalID.style.display = 'block';
+}
+
+function closeModal(){
+    blog.style.display = 'none';
+}
+
+//event listeners
+rightSide.addEventListener('mouseenter', () => {
+    rightSide.classList.add('hover-right');
+    rightSide.classList.remove('hover-off');
+});
+
+rightSide.addEventListener('mouseleave', () => {
+    rightSide.classList.remove('hover-right');
+    rightSide.classList.add('hover-off');
+});
+
+btnPortfolio.addEventListener('click', () => {
+    portfolio.classList.add('portfolio-left-fadein');
+    btnClose();
+});
+
+btnBlog.addEventListener('click', () => {
+    showModal(blog);
+}, false);
+
+btnCloseModal.addEventListener('click', () => {
+    closeModal();
+});
+
+window.addEventListener('click', (e) => {
+    if(e.target == blog){
+        closeModal();
+    }
+});
